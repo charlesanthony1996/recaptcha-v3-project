b2638e90db6fc0f7191122829723978d2f792fbb
Charles Anthony <anthony@nu-it.at>
2022-05-26 01:20:35 +0200

    nodejs recaptcha

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express")
+var path = require("path")
+var bodyParser = require("body-parser")
+var request = require("request")
+
+var app = express()
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+var port = 3000;
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+app.post("/captcha", function (req, res) {
+    if (req.body["g-recaptcha-response"] === undefined || req.body["g-recaptcha-response"] === "" || req.body["g-recaptcha-response"] === null)
+    {
+        return res.json({"responseError": "something goes wrong"})
+    }
+
+    const secretKey = "6Ld5DhsgAAAAAIuG61XZl89k7EFFLmN8zxMH4UTw";
+
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+    request(verificationURL, function(error , response, body) {
+        body = JSON.parse(body);
+
+        if (body.success !== undefined && !body.success) {
+            return res.json({ "responseError": "Failed captcha verification"})
+        }
+        res.json({"responseSuccess":"success"})
+    })
+})
+
+app.listen(port, () => {
+    console.log("server running here")
+})
